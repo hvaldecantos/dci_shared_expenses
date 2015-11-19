4ef5e575edde9254717864e217b1de3ed84a8c7b
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-19 10:17:44 -0500

    Change field name: amount instead of total

--- db_schema.rb
+++ db_schema.rb
@@ -13,6 +13,6 @@ ActiveRecord::Schema.define do
 
   create_table :expenses, force: true do |t|
     t.string :description
-    t.decimal :total, :precision => 8, :scale => 2
+    t.decimal :amount, :precision => 8, :scale => 2
   end
 end
