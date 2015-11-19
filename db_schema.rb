76f53efffa133389f6a6f689ffa5ff23d321c273
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-19 10:07:56 -0500

    Add User and Expense models

---
+++ db_schema.rb
@@ -0,0 +1,18 @@
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name
+  end
+
+  create_table :expenses, force: true do |t|
+    t.string :description
+    t.decimal :total, :precision => 8, :scale => 2
+  end
+end
