fe936fab4ed7fb25d0510971e70ac38272b4e742
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-20 11:05:31 -0500

    Fix balance computation

--- compute_balance_context.rb
+++ compute_balance_context.rb
@@ -20,7 +20,8 @@ class ComputeBalanceContext
     execute_in_context do
       debtors = accountant.compute_debtors
       creditors = accountant.compute_creditors
-      balance = debtors.merge(creditors){|user, deb, cred| deb - cred }
+      balance = debtors.merge(creditors).inject({}){|h, (k,v)| h.merge( k => 0 )}
+      balance = balance.merge(debtors).merge(creditors) {|user, deb, cred| deb - cred }
       balance.each {|k,v| puts "user: #{k} amount: #{v}"}
     end
   end
