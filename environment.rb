7711aa91f5656c2b715649afb08e5eb0a5d1d0e8
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-27 17:09:04 -0500

    Add Payment model

--- environment.rb
+++ environment.rb
@@ -2,6 +2,7 @@ require './db_schema'
 require './user'
 require './expense'
 require './share'
+require './payment'
 
 adele = User.new(name: "Adele")
 dan = User.new(name: "Dan")
@@ -18,7 +19,18 @@ beer.save
 p User.all
 p Expense.all
 
+# sharing a pizza
 adele.shares.create(expense: pizza,amount: 7.10)
 Share.create(user: dan, expense: pizza,amount: 7.10)
 
+# sharing beer
+adele.shares.create(expense: beer, amount: 3.25)
+dan.shares.create(expense: beer, amount: 3.25)
+alan.shares.create(expense: beer, amount: 3.25)
+
 p Share.all
+
+adele.payments.create(expense: pizza)
+dan.payments.create(expense: beer)
+
+p Payment.all
--- expense.rb
+++ expense.rb
@@ -3,4 +3,5 @@ require './db_schema'
 
 class Expense < ActiveRecord::Base
   has_many :shares, inverse_of: :expense
+  has_one :payment, inverse_of: :expense
 end
