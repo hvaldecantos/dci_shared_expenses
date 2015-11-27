c545e800864cdfba12d2e504be2c529f4f136784
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-11-27 16:58:50 -0500

    Add Share model to register shared expenses

--- environment.rb
+++ environment.rb
@@ -1,13 +1,24 @@
 require './db_schema'
 require './user'
 require './expense'
+require './share'
 
-User.new(name: "Adele").save
-User.new(name: "Dan").save
-User.new(name: "Alan").save
+adele = User.new(name: "Adele")
+dan = User.new(name: "Dan")
+alan = User.new(name: "Alan")
+adele.save
+dan.save
+alan.save
 
-Expense.new(description: "Pizza", amount: 14.20).save
-Expense.new(description: "Beer", amount: 9.75).save
+pizza = Expense.new(description: "Pizza")
+beer = Expense.new(description: "Beer")
+pizza.save
+beer.save
 
 p User.all
 p Expense.all
+
+adele.shares.create(expense: pizza,amount: 7.10)
+Share.create(user: dan, expense: pizza,amount: 7.10)
+
+p Share.all
