74adc0a869ab9acc2736ef2a268ec21f05146dad
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-01 10:57:52 -0500

    fix User Expense relationship

--- user.rb
+++ user.rb
@@ -2,7 +2,7 @@ require 'active_record'
 require './db_schema'
 
 class User < ActiveRecord::Base
-  has_many :paid_expenses, inverse_of: :payer, foreign_key: :user_id, class_name: :Expense
+  has_many :paid_expenses, inverse_of: :payer, class_name: :Expense
   has_many :shared_expenses, :through => :shares, :source => :expense
   has_many :shares, inverse_of: :user
 end
