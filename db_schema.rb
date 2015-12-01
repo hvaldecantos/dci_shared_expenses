1fbb5bf6ec89bd4ba489bc43f8b1fc46e27f38ea
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-01 05:45:53 -0500

    Create User data model

---
+++ db_schema.rb
@@ -0,0 +1,13 @@
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:'
+)
+
+ActiveRecord::Schema.define do
+  create_table :users, force: true do |t|
+    t.string :name
+  end
+end
