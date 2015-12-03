63364e2c15182797aa06d078dd8fdf3e93f0b129
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-02 21:58:31 -0500

    add Payment data model

--- environment.rb
+++ environment.rb
@@ -1,6 +1,7 @@
 require './user'
 require './expense'
 require './share'
+require './payment'
 
 adele = User.create name: "Adele"
 dan = User.create name: "Dan"
@@ -13,7 +14,19 @@ ticket = Expense.create description: "Ticket"
 p User.all
 p Expense.all
 
+# sharing a pizza
 adele.shares.create(expense: pizza, amount: 7.10)
 Share.create(user: dan, expense: pizza, amount: 7.10)
 
+# sharing beer
+adele.shares.create(expense: beer, amount: 3.25)
+dan.shares.create(expense: beer, amount: 3.25)
+alan.shares.create(expense: beer, amount: 3.25)
+
 p Share.all
+
+# making payments
+adele.payments.create(expense: pizza)
+dan.payments.create(expense: beer)
+
+p Payment.all
