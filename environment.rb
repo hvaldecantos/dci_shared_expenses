adeda14d94874e125b487740e1d9013a1b734fdb
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-02 21:49:56 -0500

    add Share data model

--- environment.rb
+++ environment.rb
@@ -1,5 +1,6 @@
 require './user'
 require './expense'
+require './share'
 
 adele = User.create name: "Adele"
 dan = User.create name: "Dan"
@@ -11,3 +12,8 @@ ticket = Expense.create description: "Ticket"
 
 p User.all
 p Expense.all
+
+adele.shares.create(expense: pizza, amount: 7.10)
+Share.create(user: dan, expense: pizza, amount: 7.10)
+
+p Share.all
