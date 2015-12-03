c8fd31a6fff3dc4d8f131dbddf23b4642f9d2796
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2015-12-03 12:39:32 -0500

    add PrintDebtorsContext

--- payer_role.rb
+++ payer_role.rb
@@ -8,4 +8,16 @@ module PayerRole
     SplitExpenseContext::execute(context.expense.id, context.payee_ids, context.total_amount)
     context.payer.payments.create(expense: context.expense)
   end
+  def compute_debtors
+    debtors = {}
+    context.paid_expenses.each do |p|
+      p.expense.shares.each do |s|
+        next if s.user == self
+        debtors[s.user.id] ||= { name: "", amount: 0.0 }
+        debtors[s.user.id][:name] = s.user.name
+        debtors[s.user.id][:amount] += s.amount
+      end
+    end
+    debtors
+  end
 end
